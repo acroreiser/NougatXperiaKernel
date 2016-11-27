9e880cf290133ec20e24099ce8c6a57fec44efca
Ярик Звезда <acroreiser@gmail.com>
2016-11-27 15:27:53 +0200

    mm: page-writeback: readjust hardcodeable dirty{_background}_ratio values

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -72,7 +72,7 @@ static long ratelimit_pages = 32;
  * Start background writeback (via writeback threads) at this percentage
  */
 #ifdef CONFIG_DECREASE_DIRTY_RATIO
-int dirty_background_ratio = 2;
+int dirty_background_ratio = 3;
 #else
 int dirty_background_ratio = 10;
 #endif
@@ -92,7 +92,7 @@ int vm_highmem_is_dirtyable;
  * The generator of dirty data starts writeback at this percentage
  */
 #ifdef CONFIG_DECREASE_DIRTY_RATIO
-int vm_dirty_ratio = 4;
+int vm_dirty_ratio = 10;
 #else
 int vm_dirty_ratio = 20;
 #endif
